bb3180c919f319222e7885a985e457725add5c42
g1alexander <alexlds26@gmail.com>
2022-11-02 21:00:00 -0500

    feat: stack

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        
+        VStack (alignment: .leading, spacing: 20){
+            Text("Hola mundo").padding()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+                .padding()
+            
+            Spacer()
+            
+            ZStack{
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.gray)
+                    .padding()
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.green)
+                    .padding(40)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.blue)
+                    .padding(60)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.red)
+                    .padding(80)
+            }
+            
+            Spacer()
+            
+            HStack{
+                Text("hey bro")
+                    .padding()
+                    .background(Color.gray)
+                
+                Spacer()
+                
+                Text("siiiuuuu")
+                    .padding()
+                    .background(Color.gray)
+            }.padding()
+            
+            
+            Text("")
+            Text("")
+            Text("")
+            Text("")
+            
+            // en un vStack solo puedes devolver 10 vistas
+            
+        }.padding().background(Color.yellow)
+        
     }
 }
 
